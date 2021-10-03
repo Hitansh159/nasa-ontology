d39d7045b87fc04db19a27c287b4e2f656880566
Shreyas30 <jshreyas12@gmail.com>
2021-10-03 17:18:50 +0530

    tfidf_knn.py updated

---
+++ engines/tfidf_engine.py
@@ -0,0 +1,25 @@
+import pickle
+import numpy as np
+import re
+
+
+def clean_sentence(s):
+    temp = []
+    regex = r"[^a-zA-Z ]"
+    subst = " "
+    result = re.sub(regex, subst, s)
+    sen = []
+    for j in result.split():
+        sen.append(j)
+    return (" ".join(sen))
+
+
+def search(query):
+    vectorizer = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
+    x = clean_sentence(query)
+    x = vectorizer.transform([x])
+    knn = pickle.load(open("tfidf_knn.pkl", "rb"))
+    neighbours = knn.kneighbors(x[0], 200)
+    distances = (np.max(neighbours[0]) - neighbours[0]) / \
+        ((np.max(neighbours[0])-(np.min(neighbours[0]))))
+    return (list(zip(neighbours[1][0].tolist(), distances[0].tolist())))
